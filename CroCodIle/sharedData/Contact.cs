1fdca6ef7698af66db94e3d9eb9d193b1137c170
cpearson1990_cp <SND\cpearson1990_cp>
2010-08-19 16:14:54 +0000

    Modifications made for testing object-passing.

---
+++ CroCodIle/sharedData/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sharedData
+{
+    [Serializable]
+    public class Contact
+    {
+        public string Name { get; set; }
+    }
+}
